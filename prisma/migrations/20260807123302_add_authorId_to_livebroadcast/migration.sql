a85dd6e5895e5c248cb3b46bd8444a0b69846ebd
Mc-Pentester <mcintoshfr@gmail.com>
2026-08-08 17:21:57 -0400

    Suppression de migration

--- prisma/migrations/20260807123302_add_authorId_to_livebroadcast/migration.sql
+++
@@ -1,5 +0,0 @@
--- Add authorId column to LiveBroadcast
-ALTER TABLE "LiveBroadcast" ADD COLUMN "authorId" TEXT NOT NULL;
-
--- Add relation to User model
-ALTER TABLE "LiveBroadcast" ADD CONSTRAINT "LiveBroadcast_authorId_fkey" FOREIGN KEY ("authorId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
